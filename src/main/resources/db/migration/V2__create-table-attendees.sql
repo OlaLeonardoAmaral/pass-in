219ad1775896da7433dcc21f31387ccae491b399
Leonardo <leonardo3442@hotmail.com>
2024-04-02 12:09:27 -0300

    create: database migrations

---
+++ src/main/resources/db/migration/V2__create-table-attendees.sql
@@ -0,0 +1,8 @@
+CREATE TABLE attendees (
+    attendee_id VARCHAR(255) NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    event_id VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT attendees_event_id_fkey FOREIGN KEY (event_id) REFERENCES events (event_id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
