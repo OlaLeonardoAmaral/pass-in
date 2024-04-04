ec9b3e88a0ecb98d1ee238c8505b94c25a905d80
Leonardo <leonardo3442@hotmail.com>
2024-04-04 00:45:19 -0300

    feat: findByAttendeeId and findByEventId

--- src/main/java/br/com/amaral/passin/repositories/AttendeeRepository.java
+++ src/main/java/br/com/amaral/passin/repositories/AttendeeRepository.java
@@ -1,9 +1,11 @@
 package br.com.amaral.passin.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.amaral.passin.domain.attendee.Attendee;
 
 public interface AttendeeRepository extends JpaRepository<Attendee, String> {
-    
+    List<Attendee> findByEventEventId(String eventId);
 }
--- src/main/java/br/com/amaral/passin/repositories/CheckInRepository.java
+++ src/main/java/br/com/amaral/passin/repositories/CheckInRepository.java
@@ -1,9 +1,13 @@
 package br.com.amaral.passin.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.amaral.passin.domain.checkin.CheckIn;
 
 public interface CheckInRepository extends JpaRepository<CheckIn, Integer> {
+
+    Optional<CheckIn> findByAttendeeAttendeeId(String AttendeeId);
     
 }
