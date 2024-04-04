69ff91d19a394802eb5c979d0a6b687ee3d82a66
Leonardo <leonardo3442@hotmail.com>
2024-04-04 00:52:32 -0300

    refactor: AttendeesDTO

--- src/main/java/br/com/amaral/passin/dto/attendee/AttendeesListResponseDTO.java
+++ src/main/java/br/com/amaral/passin/dto/attendee/AttendeesListResponseDTO.java
@@ -2,5 +2,5 @@ package br.com.amaral.passin.dto.attendee;
 
 import java.util.List;
 
-public record AttendeesListResponseDTO(List<AttendeeDetails> attendees) {
+public record AttendeesListResponseDTO(List<AttendeeDetailsDTO> attendees) {
 }
